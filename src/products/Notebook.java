a428010553707833ee7f160b073c837968ee05fb
Aurora Scalici <aurorascalici1899@icloud.com>
2024-02-28 21:54:05 +0100

    get set2

--- src/products/Notebook.java
+++ src/products/Notebook.java
@@ -5,6 +5,7 @@ public class Notebook extends Product {
     public Notebook(String device, String brand, String model, String description, float display, float storage,
             float salePrice, float purchasePrice, float id) {
         super(device, brand, model, description, display, storage, salePrice, purchasePrice, id);
+
         getDevice();
         getBrand();
         getModel();
@@ -14,5 +15,14 @@ public class Notebook extends Product {
         getSalePrice();
         getPurchasePrice();
         getId();
+        setDevice(device);
+        setBrand(brand);
+        setModel(model);
+        setDescription(description);
+        setDisplay(display);
+        setStorage(storage);
+        setSalePrice(salePrice);
+        setPurchasePrice(purchasePrice);
+        setId(id);
     }
 }
--- src/products/Smartphone.java
+++ src/products/Smartphone.java
@@ -5,6 +5,7 @@ public class Smartphone extends Product {
     public Smartphone(String device, String brand, String model, String description, float display, float storage,
             float salePrice, float purchasePrice, float id) {
         super(device, brand, model, description, display, storage, salePrice, purchasePrice, id);
+        
         getDevice();
         getBrand();
         getModel();
@@ -14,5 +15,14 @@ public class Smartphone extends Product {
         getSalePrice();
         getPurchasePrice();
         getId();
+        setDevice(device);
+        setBrand(brand);
+        setModel(model);
+        setDescription(description);
+        setDisplay(display);
+        setStorage(storage);
+        setSalePrice(salePrice);
+        setPurchasePrice(purchasePrice);
+        setId(id);
     }
 }
