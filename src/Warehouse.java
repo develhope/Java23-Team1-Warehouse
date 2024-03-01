39242f86b1322eb080a04271e053080e601a6e5a
SIlvioVutea <silviovutea@gmail.com>
2024-03-01 10:40:42 +0100

    tried to build a constructor for a List of products

--- src/Warehouse.java
+++ src/Warehouse.java
@@ -3,17 +3,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Warehouse {
-    private List<Product> productsInStock;
-
-
-    public Warehouse(Product[]products) {
-        this.productsInStock = new ArrayList<Product>();
+    private List<Product> productsInStock = new ArrayList<Product>();
+    public void addDevice(Product product) {
+        productsInStock.add(device);
     }
 
     public List<Product> getProductsInStock() {
         return productsInStock;
     }
-
 }
 
 
