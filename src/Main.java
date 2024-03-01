717c1e9e96802ca4a4ee22024027296807d1fa0a
Ttamer <mericomattia@gmail.com>
2024-03-01 10:58:52 +0100

    just for pull updated code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import products.Devices;
+import products.Product;
+
 public class Main {
 
     public static void main(String[] args) {
-
     }
 
 }
--- src/ResearchMethod.java
+++ src/ResearchMethod.java
@@ -1,4 +1,15 @@
+import products.Product;
+
+import java.util.Iterator;
+import java.util.List;
+
 abstract class ResearchMethod {
     void searchByType (){};
-    void printAllDevices () {};
+    void printDevices (List<Product> productDaDefinire) {
+        Iterator<Product> iterator = productDaDefinire.iterator();
+        while (iterator.hasNext()) {
+            if ()
+            System.out.println(iterator.next());
+        }
+    };
 }
