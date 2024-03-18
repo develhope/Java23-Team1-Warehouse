3e1e08b9c758f1f9f17d0e13a239a449cd02cf84
Ttamer <mericomattia@gmail.com>
2024-03-18 10:21:20 +0100

    created class FilterByModel implementation of ProductFilter

---
+++ src/FilterByModel.java
@@ -0,0 +1,16 @@
+import products.Product;
+
+import java.util.Objects;
+
+public class FilterByModel implements ProductFilter{
+    private String input;
+
+    public FilterByModel (String input) {
+        this.input = input.toLowerCase();
+    }
+
+    @Override
+    public boolean isInFilter(Product product) {
+        return Objects.equals(input, product.getModel().toLowerCase());
+    }
+}
